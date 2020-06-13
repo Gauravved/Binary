46aa8fc3d6b35504d23c3f035ac9587da2151f10
Gauravved <Gauravved@github.com>
2020-06-13 17:37:53 +0530

    Decimal to binary converter

---
+++ binary.c
@@ -0,0 +1,206 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() 
+{
+	long ar[32];
+	int n,i,a[5]={0,0,0,0,0},m,cnt[10],j,k,grt=0;
+	double f;
+	char choice;
+	system("CLS");
+	int choose;
+	do
+	{
+		printf("-------------------------------------- Number Conversion --------------------------------------");
+		printf("\n1> Decimal to Binary: \n2> Decimal to Octal: \n3> Decimal to Hexadecimal:\nEnter your choice:");
+		scanf("%d",&choose);
+		if(choose==1)
+		{
+			printf("\nEnter the number to convert:");
+			scanf("%lf",&f);
+			n=f;
+			for(i=0;n>0;i++)
+			{
+				ar[i]=n%2;
+				n=n/2;
+			}
+			m=i;
+			k=0;
+			do
+			{	
+				if(f>=1)
+				{
+					f=f-(int)f;
+				}	
+				f=f*2;
+				a[k]=(int)f;
+				k++;
+			}while(f>=0 && k<4);
+			printf("The binary number is :");
+			for(i=m-1;i>=0;i--)
+			{
+				grt=0;
+				printf("%d",ar[i]);
+				if(ar[i]==1)
+				{
+					j=i-1;
+					grt=0;
+					while(ar[j]!=1 &&j>=0)
+					{ 
+						grt++;
+						j--;
+					}
+					if(j<0)
+					{
+						grt=0;
+					}
+				}
+				cnt[i]=grt;
+			}
+			printf(".");
+			k=0;
+			while(k<4)
+			{
+				printf("%d",a[k]);
+				k++;
+			}
+			grt=cnt[0];
+			for(i=m-1;i>=0;i--)
+			{
+				if(grt<cnt[i])
+				{
+					grt=cnt[i];
+				}
+			}
+			if(grt==0)
+			{ 
+				printf("\nThere is no binary gap");
+			}
+			else
+				printf("\nThe longest binary gap is %d",grt);
+		}
+		else if(choose==2)
+		{
+			printf("\nEnter the number to convert:");
+			scanf("%lf",&f);
+			n=f;
+			for(i=0;n>0;i++)
+			{
+				ar[i]=n%8;
+				n=n/8;
+			}
+			k=0;
+			do
+			{	
+				if(f>=1)
+				{
+					f=f-(int)f;
+				}	
+				f=f*8;
+				a[k]=(int)f;
+				k++;
+			}while(f>=0 && k<4);
+			printf("The octal number is :");
+			m=i;
+			for(i=m-1;i>=0;i--)
+			{
+				printf("%d",ar[i]);
+			}
+			printf(".");
+			for(k=0;k<4;k++)
+			{
+				printf("%d",a[k]);
+			}
+		}
+		else if(choose==3)
+		{
+			printf("\nEnter the number to convert:");
+			scanf("%lf",&f);
+			n=f;
+			for(i=0;n>0;i++)
+			{
+				ar[i]=n%16;
+				n=n/16;		
+			}
+			k=0;
+			do
+			{	
+				if(f>=1)
+				{	
+					f=f-(int)f;
+				
+				}	
+				f=f*16.0;
+				a[k]=(int)f;
+				k++;
+			}while(f>=0 && k<4);
+			printf("The hexadecimal number is :");
+			m=i;
+			for(i=m-1;i>=0;i--)
+			{
+				if(ar[i]==10)
+				{
+					printf("A");
+				}
+				else if(ar[i]==11)
+				{
+					printf("B");
+				}
+				else if(ar[i]==12)
+				{
+					printf("C");
+				}
+				else if(ar[i]==13)
+				{
+					printf("D");
+				}
+				else if(ar[i]==14)
+				{
+					printf("E");
+				}
+				else if(ar[i]==15)
+				{
+					printf("F");
+				}
+				else
+					printf("%d",ar[i]);
+			}
+			printf(".");
+			for(k=0;k<4;k++)
+			{	if(a[k]==10)
+				{
+					printf("A");
+				}
+				else if(a[k]==11)
+				{
+					printf("B");
+				}
+				else if(a[k]==12)
+				{
+					printf("C");
+				}
+				else if(a[k]==13)
+				{
+					printf("D");
+				}
+				else if(a[k]==14)
+				{
+					printf("E");
+				}
+				else if(a[k]==15)
+				{
+					printf("F");
+				}
+				else
+					printf("%d",a[k]);
+			}
+		}
+		else
+		{
+			printf("Please recheck the number!!!!");
+		}
+		printf("\n\nDo you want to continue? [y/n] :");
+		scanf("%s",&choice);
+		system("CLS");
+	}while(choice=='y'||choice=='Y');
+	return 0;
+}
